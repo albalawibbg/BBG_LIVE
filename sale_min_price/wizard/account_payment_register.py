9b1e51da85fff830c8e8d1ab8a5957b141fbb5b6
AWAD <awad-archer>
2025-04-07 16:50:39 +0200

    sale_min_price

---
+++ sale_min_price/wizard/account_payment_register.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+from odoo import api, models,fields
+
+
+class AccountPaymentRegister(models.TransientModel):
+    """ Adding allowed journal in functionality"""
+    _inherit = 'account.payment.register'
+
+    employee_id = fields.Many2one(
+        'hr.employee',
+        string="Employee"
+    )
+    def _create_payment_vals_from_wizard(self, batch_result):
+        payment_vals = super()._create_payment_vals_from_wizard(batch_result)
+        payment_vals.update({'employee_id':self.employee_id.id})
+        return payment_vals
