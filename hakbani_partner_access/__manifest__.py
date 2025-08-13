29a1ff932f88c666eaa01ea5270451357e3eeacc
AWAD <awad-archer>
2025-08-13 13:05:08 +0300

    hakbani_partner_access,af_card_product

---
+++ hakbani_partner_access/__manifest__.py
@@ -0,0 +1,21 @@
+{
+    'name': 'Hakbani - Partner access restrictions',
+    'version': '1.0',
+    'summary': 'Hakbani Module for access restrictions on Partner',
+    'author': 'Hakbani IT',
+    'depends': [
+        'base',
+        'account',
+        'account_followup',
+        'purchase',
+        'account_reports',
+        'sale',
+    ],
+    'data': [
+        'security/security.xml'
+        'views/partner.xml'
+    ],
+    'installable': True,
+    'application': False,
+    'license': 'LGPL-3',
+}
