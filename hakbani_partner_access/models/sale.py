c583aa5560db5305dbd4cea8190aff03536893bc
AWAD <awad-archer>
2025-09-18 19:12:39 +0300

    hakbani_partner_access

--- hakbani_partner_access/models/sale.py
+++ hakbani_partner_access/models/sale.py
@@ -13,10 +13,10 @@ class Sale(models.Model):
                 if view_type == 'form':
                     nodes_form = doc.xpath("//form//field[@name='user_id']")
                     for node in nodes_form:
-                        node.set('edit', '1')
+                        node.set('readonly', '1')
                     nodes_form = doc.xpath("//form//field[@name='team_id']")
                     for node in nodes_form:
-                        node.set('edit', '1')
+                        node.set('readonly', '1')
 
                     res['views'][view_type]['arch'] = etree.tostring(doc)
         else:
@@ -25,10 +25,10 @@ class Sale(models.Model):
                 if view_type == 'form':
                     nodes_form = doc.xpath("//form//field[@name='user_id']")
                     for node in nodes_form:
-                        node.set('edit', '0')
+                        node.set('readonly', '0')
                     nodes_form = doc.xpath("//form//field[@name='team_id']")
                     for node in nodes_form:
-                        node.set('edit', '0')
+                        node.set('readonly', '0')
                 res['views'][view_type]['arch'] = etree.tostring(doc)
         return res
 
