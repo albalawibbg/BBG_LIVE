ff4ff3069aa6104dc1f73037cf7e06c68d908ead
AWAD <awad-archer>
2025-09-18 18:52:52 +0300

    hakbani_partner_access

---
+++ hakbani_partner_access/models/invoice.py
@@ -0,0 +1,35 @@
+from odoo import api, fields, models, exceptions
+from odoo.tools.safe_eval import safe_eval
+from lxml import etree
+class Partner(models.Model):
+    _inherit = 'account.move'
+
+    @api.model
+    def get_views(self, views, options=None):
+        res = super().get_views(views, options)
+        has_edit_access = self.env.user.has_group('hakbani_partner_access.group_person_team_edit')
+        if has_edit_access:
+            for view_type in res['views']:
+                doc = etree.XML(res['views'][view_type]['arch'])
+                if view_type == 'form':
+                    nodes_form = doc.xpath("//form//field[@name='invoice_user_id']")
+                    for node in nodes_form:
+                        node.set('edit', '1')
+                    nodes_form = doc.xpath("//form//field[@name='team_id']")
+                    for node in nodes_form:
+                        node.set('edit', '1')
+
+                    res['views'][view_type]['arch'] = etree.tostring(doc)
+        else:
+            for view_type in res['views']:
+                doc = etree.XML(res['views'][view_type]['arch'])
+                if view_type == 'form':
+                    nodes_form = doc.xpath("//form//field[@name='invoice_user_id']")
+                    for node in nodes_form:
+                        node.set('edit', '1')
+                    nodes_form = doc.xpath("//form//field[@name='team_id']")
+                    for node in nodes_form:
+                        node.set('edit', '1')
+                res['views'][view_type]['arch'] = etree.tostring(doc)
+        return res
+
---
+++ hakbani_partner_access/models/sale.py
@@ -0,0 +1,34 @@
+from odoo import api, fields, models, exceptions
+from odoo.tools.safe_eval import safe_eval
+from lxml import etree
+class Sale(models.Model):
+    _inherit = 'sale.order'
+    @api.model
+    def get_views(self, views, options=None):
+        res = super().get_views(views, options)
+        has_edit_access = self.env.user.has_group('hakbani_partner_access.group_person_team_edit')
+        if has_edit_access:
+            for view_type in res['views']:
+                doc = etree.XML(res['views'][view_type]['arch'])
+                if view_type == 'form':
+                    nodes_form = doc.xpath("//form//field[@name='user_id']")
+                    for node in nodes_form:
+                        node.set('edit', '1')
+                    nodes_form = doc.xpath("//form//field[@name='team_id']")
+                    for node in nodes_form:
+                        node.set('edit', '1')
+
+                    res['views'][view_type]['arch'] = etree.tostring(doc)
+        else:
+            for view_type in res['views']:
+                doc = etree.XML(res['views'][view_type]['arch'])
+                if view_type == 'form':
+                    nodes_form = doc.xpath("//form//field[@name='user_id']")
+                    for node in nodes_form:
+                        node.set('edit', '1')
+                    nodes_form = doc.xpath("//form//field[@name='team_id']")
+                    for node in nodes_form:
+                        node.set('edit', '1')
+                res['views'][view_type]['arch'] = etree.tostring(doc)
+        return res
+
