f2d0b1c1c6b8ea1d45fc343e95c7453d43392b16
AWAD <awad-archer>
2025-10-19 21:16:29 +0300

    sale_custom

--- partner_aged_receivable_filters/models/model.py
+++ partner_aged_receivable_filters/models/model.py
@@ -10,14 +10,14 @@ class Users(models.Model):
     def _name_search(self, name, domain=None, operator='ilike', limit=None, order=None):
         domain = domain or []
         _logger.info(">>>>>>>>>>>>>>>>>>>>>{}".format(self.env.context))
-        if domain == ['!', ['id', 'in', []]] and not self.env.user.has_group('partner_aged_receivable_filters.group_show_aged_sp') and self.env.user.has_group('sale_custom.group_specific_sales_persons'):
+        if self.env.context.get('report_id',0) == self.env.ref('account_reports.aged_receivable_report').id and not self.env.user.has_group('partner_aged_receivable_filters.group_show_aged_sp') and self.env.user.has_group('sale_custom.group_specific_sales_persons'):
             domain = expression.AND([[('id', 'in', self.env.user.sales_users.ids)], domain])
         return self._search(domain, limit=limit, order=order)
     @api.model
     def _search(self, domain, offset=0, limit=None, order=None, access_rights_uid=None):
         domain = domain or []
         _logger.info("************************{}".format(self.env.context))
-        if not domain or (domain and any(d[0] == 'id' and d[1] == [] and len(domain) == 1 for d in domain )) and not self.env.user.has_group('partner_aged_receivable_filters.group_show_aged_sp')  and self.env.user.has_group('sale_custom.group_specific_sales_persons'):
+        if not domain or (domain and any(d[0] == 'id' and d[1] == [] and len(domain) == 1 for d in domain )) or self.env.context.get('report_id',0) == self.env.ref('account_reports.aged_receivable_report').id  and not self.env.user.has_group('partner_aged_receivable_filters.group_show_aged_sp')  and self.env.user.has_group('sale_custom.group_specific_sales_persons'):
             domain = expression.AND([[('id', 'in', self.env.user.sales_users.ids)], domain])
         return super()._search(domain, offset, limit, order, access_rights_uid)
 class AccountMoveLine(models.Model):
