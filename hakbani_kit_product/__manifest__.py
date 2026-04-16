7fa46837a5ed90c68a282e01adfdf4f481c3f4d1
User19490 <70704160+User19490@users.noreply.github.com>
2026-04-16 16:37:42 +0300

    hakbani_kit_product
    hakbani_kit_product

--- hakbani_kit_product/__manifest__.py
+++ hakbani_kit_product/__manifest__.py
@@ -1,15 +1,19 @@
 # -*-coding: utf-8 -*-
 {
-    "name": "Hakbani-IT Stock Product",
-    "summary": "Hakbani-IT Stock Product",
+    "name": "Hakbani-IT Kit Product",
+    "summary": "Hakbani-IT Kit Product",
     "version": "17.0.0.0",
     "category": "Sale",
     "author": "Hakbani IT",
     'website': 'https://www.nutechits.com',
     "license": "AGPL-3",
-    "depends": ["product", "sale","stock","warehouse_stock_restrictions"],
+    "depends": ["product", "sale"],
     "data": [
+        'security/ir.model.access.csv',
+        # 'views/mrp_bom.xml',
         'views/sale_order.xml',
+        # 'views/product_template.xml',
+        'views/warehouse_product_wizard.xml',
     ],
     'installable': True,
     'application': True,
--- hakbani_kit_product/models/sale.py
+++ hakbani_kit_product/models/sale.py
@@ -8,12 +8,6 @@ class SaleOrderLine(models.Model):
 
     def show_product_stock_wizard(self):
         ctx = {'product_id': self.product_id.id, 'readonly': True,'create': False}
-        if self.env.user.has_group('warehouse_stock_restrictions.group_restrict_warehouse') and self.env.user.restrict_locations:
-            domain =  [('product_id', '=', self.product_id.id), ('location_id.usage', '=', 'internal'),
-                       ('location_id', 'in', self.env.user.stock_location_ids.ids)]
-        else:
-            domain = [('product_id', '=', self.product_id.id), ('location_id.usage', '=', 'internal')]
-
         return {
             'name': "Warehouse Product Stock",
             'view_mode': 'tree',
@@ -21,6 +15,6 @@ class SaleOrderLine(models.Model):
             'type': 'ir.actions.act_window',
             'view_id': self.env.ref('stock.view_stock_quant_tree').id,
             'context': ctx,
-            'domain':domain,
+            'domain': [('product_id', '=', self.product_id.id), ('location_id.usage','=', 'internal')],
             'target': 'new',
         }
