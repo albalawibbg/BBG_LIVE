1f569bf817633800b0c819e1e1b0ed491d9f027b
User19490 <70704160+User19490@users.noreply.github.com>
2026-06-25 15:39:42 +0300

    hakbani_petty_cash

---
+++ hakbani_petty_cash/models/sale_order.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+from odoo import api, fields, models, _
+from odoo.exceptions import UserError, ValidationError
+
+
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+
+    conract_no = fields.Char(string="Contract No")
+
+
+class SaleAdvancePaymentInv(models.TransientModel):
+    _inherit = "sale.advance.payment.inv"
+
+    def create_invoices(self):
+        sale_orders = self.env['sale.order'].browse(self._context.get('active_ids', []))
+        res = super(SaleAdvancePaymentInv, self).create_invoices()
+        print ('sale_orders',res)
+        for inv in sale_orders.invoice_ids:
+            inv.client_order_ref = sale_orders.client_order_ref
+            inv.conract_no = sale_orders.conract_no
+
+        return res
